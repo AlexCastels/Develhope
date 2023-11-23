88a5607be9172d99c861a370ddca7121efd77b4b
Alessandro <alessandrofrancescocastelli@outlook.it>
2023-11-23 14:01:40 +0100

    Esercizio String 2

---
+++ js_template_string2/es.js
@@ -0,0 +1,10 @@
+//Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+let frase = `Utilizza una template string  
+per creare una stringa 
+su più righe che includa 
+interruzioni di riga`
+
+console.log(frase)
+
+//intendavamo il fatto di portare a capo una stringa, no?
