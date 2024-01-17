dbd03fb2723c2d34b6affc938a4802be4ef934d4
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-01-17 11:17:05 +0100

    Components 2

---
+++ components2/src/App.jsx
@@ -0,0 +1,12 @@
+import { HelloWorld } from "./components/HelloWorld";
+import { Message } from "./components/Message";
+
+export function App(){
+    return (
+        <div>
+            <HelloWorld/>
+            <Message/>
+        </div>
+        
+    )
+}
