bdc5260c78b3d9ff9a2760504f47f5029a8d645a
Alessandro <alessandrofrancescocastelli@outlook.it>
2024-02-29 12:05:12 +0100

    modifiche esercizi 3-4

--- 3-insertInto-table/dump.sqlite.sql
+++ 3-insertInto-table/dump.sqlite.sql
@@ -1,7 +1,25 @@
 -- TABLE
-CREATE TABLE Books (book_id INTEGER PRIMARY KEY, title TEXT, author TEXT,genre TEXT, published_year INT, isbn INT, price INT, rating INT, stock_count INT, publisher_name TEXT, pages INT);
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
- 
+CREATE TABLE Books (
+    book_id INTEGER PRIMARY KEY, 
+    title TEXT, 
+    author TEXT,
+    genre TEXT, 
+    published_year INT, 
+    isbn INT, price INT, 
+    rating INT, 
+    stock_count INT, 
+    publisher_name TEXT, 
+    pages INT
+);
+
+
+INSERT INTO Books (book_id , title , author , genre , published_year , isbn , price , rating , stock_count , publisher_name , pages)
+VALUES
+(1, "A Journey to the Center" , 'Jules Verne' , 'Adventure' , 1864 , 1234567890123 , 12.99 ,  4.5 , 10 , 'Verne Publishing' , 350) , 
+(2 , "War and Peace" , 'Leo Tolstoy' , 'Historical' , 1869 , 1234567890124 , 14.99 , 4.8 , 5 , 'Tolstoy Prints' , 1200)
+(3 , "Whispers of the Wind" , 'Amelia Blackburn' , 'Romance' , 1982 , 1234567890125 , 9.99 , 4.2 , 20 , 'Blackburn House' , 275) ,
+(4 , "The Galactic Odyssey" , 'Orion Starfield' , 'Fiction' ,  2005 , 1234567890126 , 19.99 , 4.9 , 15 , 'Nebula Press' , 450) 
+
 -- INDEX
  
 -- TRIGGER
--- 4-update-table/dump.sqlite.sql
+++ 4-update-table/dump.sqlite.sql
@@ -1,6 +1,21 @@
 -- TABLE
-CREATE TABLE Books (book_id INTEGER PRIMARY KEY, title TEXT, author TEXT,genre TEXT, published_year INT, isbn INT, price INT, rating INT, stock_count INT, publisher_name TEXT, pages INT);
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Books (
+    book_id INTEGER PRIMARY KEY, 
+    title TEXT, 
+    author TEXT,
+    genre TEXT, 
+    published_year INT, 
+    isbn INT, price INT, 
+    rating INT, 
+    stock_count INT, 
+    publisher_name TEXT, 
+    pages INT
+);
+
+--per modificare dei valori in una table
+
+UPDATE Books set price = 12.99 Where book_id = 2
+UPDATE Books set stock_count = 4 Where book_id = 2
  
 -- INDEX
  
